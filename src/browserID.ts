0f0bd2521765a4139f507f9e9b7af20631559ee0
Cat Chen <catchen@catchen.me>
2024-11-24 15:36:02 -0800

    Migration for JSR (#809)

--- src/browserID.ts
+++ src/browserID.ts
@@ -1,3 +1,19 @@
-declare function browserId(): string;
+import { v4 as uuid } from 'uuid';
+import { default as Storage } from 'versioned-storage';
+
+const STORAGE_NAME = 'browser_id';
+const STORAGE_VERSION = 1;
+const storage: Storage<string> = new Storage(STORAGE_NAME, STORAGE_VERSION);
+
+function browserId(): string {
+  const existingID = storage.read();
+  if (existingID === null || existingID === undefined) {
+    const newID: string = uuid();
+    storage.write(newID);
+    return newID;
+  } else {
+    return existingID;
+  }
+}
 
 export default browserId;

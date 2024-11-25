e3165ff1b066594db57d47b3621bbe957281013f
Cat Chen <catchen@catchen.me>
2024-11-24 23:35:50 -0800

    Added jsdoc for JSR (#813)

--- src/browserID.ts
+++ src/browserID.ts
@@ -5,6 +5,11 @@ const STORAGE_NAME = 'browser_id';
 const STORAGE_VERSION = 1;
 const storage: Storage<string> = new Storage(STORAGE_NAME, STORAGE_VERSION);
 
+/**
+ * A function that returns a consistent unique identifier for the browser (or the Node/Deno environment).
+ * The same browser will always return the same identifier.
+ * @returns {string} The unique identifier.
+ */
 function browserId(): string {
   const existingID = storage.read();
   if (existingID === null || existingID === undefined) {
